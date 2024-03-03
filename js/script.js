6df14a78da452a98bd592141626101c2aa980ce9
VitaliiZaluskii <vzaluskiy23@gmail.com>
2024-03-03 20:21:19 +0200

    v0.0.5

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
 document.addEventListener("DOMContentLoaded", (e) => {
   "use strict";
-
   const peoples = [];
-  let peopleIndex;
   const btnReg = document.querySelector(".btn__reg");
   const btnLog = document.querySelector(".btn__log");
   const btnLogOut = document.querySelector(".btn__logOut");
@@ -11,11 +9,13 @@ document.addEventListener("DOMContentLoaded", (e) => {
   const afterlogin__block = document.querySelector(".afterlogin__block");
   const borrowBox = document.querySelector(".borrow__box");
   let currentBalance = 0;
-  const balance = document.querySelector(".current__balance");
   const borrowBtn = document.querySelector(".borrow__money");
-  let operatingData = {},
-    regOperatingData = {};
+
+  const tranzactionBox = document.querySelector(".transaction__box");
+  let displayedBalance;
+  let regOperatingData = {};
   let login, pass;
+  let current__index;
   borrowBox.classList.add("hidden");
   btnReg.addEventListener("click", function () {
     document.querySelector(".sighUp-block").classList.remove("hidden");
@@ -36,6 +36,8 @@ document.addEventListener("DOMContentLoaded", (e) => {
       regOperatingData = {
         login: document.querySelector(".loginReg__input").value,
         password: document.querySelector(".passwordReg__input").value,
+        balance: 0,
+        transactions: [],
       };
       localStorage.setItem(
         document.querySelector(".loginReg__input").value,
@@ -71,15 +73,46 @@ document.addEventListener("DOMContentLoaded", (e) => {
   });
 
   borrowBtn.addEventListener("click", function () {
+    currentBalance = localStorage.getItem(current__index.balance);
     let valueFromInputBorrow = document.querySelector(
       ".borrow__money-input"
     ).value;
     valueFromInputBorrow = Number(valueFromInputBorrow);
     currentBalance += valueFromInputBorrow;
     console.log(currentBalance);
+    let balancee = localStorage.getItem(current__index);
+    balancee = JSON.parse(balancee);
+    balancee.balance += currentBalance;
+
+    const dataBorrow = new Date();
+    let transactionsObj = localStorage.getItem(current__index);
+    transactionsObj = JSON.parse(transactionsArr);
+    transactionsArr = transactionsObj.transactions;
+    transactionsArr.push(
+      `${dataBorrow.getHours()}:${dataBorrow.getMinutes()}:${dataBorrow.getSeconds()}, ${dataBorrow.getDate()}.${
+        dataBorrow.getMonth() + 1
+      }.${dataBorrow.getFullYear()}`,
+      valueFromInputBorrow
+    );
+    transactionsObj.transactions = transactionsArr;
+    let modifiedTransaction = JSON.stringify(transactionsArr);
+    localStorage.setItem(current__index, modifiedTransaction); //туть
+
+    // let tranzactionElement = document.createElement("div");
+    // tranzactionElement.innerHTML = `<div>
+    // data of tranzaction: ${dataBorrow.getHours()}:${dataBorrow.getMinutes()}:${dataBorrow.getSeconds()}, ${dataBorrow.getDate()}.${
+    //   dataBorrow.getMonth() + 1
+    // }.${dataBorrow.getFullYear()} <br>
+    // borrow amount: $${valueFromInputBorrow}
+    // </div>
+    // `;
+    // tranzactionBox.appendChild(tranzactionElement);
+
     document.querySelector(
       ".current__balance"
-    ).innerHTML = `<h2>Current balance: $${currentBalance}</h2>`;
+    ).innerHTML = `<h2>Current balance: $${balancee.balance}</h2>`;
+    let modifiedBalance = JSON.stringify(balancee);
+    localStorage.setItem(current__index, modifiedBalance);
   });
 
   submitLog.addEventListener("click", function () {
@@ -108,15 +141,18 @@ document.addEventListener("DOMContentLoaded", (e) => {
         document.querySelector(
           ".main__name"
         ).innerHTML = `<h1>Hello, dear ${login}!!!</h1>`;
+        current__index = login;
         borrowBox.classList.remove("hidden");
+        displayedBalance = localStorage.getItem(login);
+        displayedBalance = JSON.parse(displayedBalance);
         document.querySelector(
           ".current__balance"
-        ).innerHTML = `<h2>Current balance: $${currentBalance}</h2>`;
+        ).innerHTML = `<h2>Current balance: $${displayedBalance.balance}</h2>`;
+        console.log(login.balance);
+        document.querySelector(".borrow__money-input").textContent = "";
       }, 2000);
     }
   });
-
-  borrowBox.addEventListener("click", function () {});
 
   btnLogOut.addEventListener("click", function () {
     afterlogin__block.classList.add("hidden");
