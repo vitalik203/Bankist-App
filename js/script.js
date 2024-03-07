e88ecb6731ba69d895280a5e7b46b5e9e6347902
Zaluskii Vitalii <vzaluskiy23@gmail.com>
2024-03-07 19:17:40 +0200

    v0.1.0

--- js/script.js
+++ js/script.js
@@ -25,43 +25,52 @@ document.addEventListener("DOMContentLoaded", (e) => {
   });
   submitReg.addEventListener("click", function () {
     if (
-      document.querySelector(".passwordReg__input").value ===
-      document.querySelector(".repeatPasswordReg__input").value
+      document.querySelector(".loginReg__input").value === "" ||
+      document.querySelector(".passwordReg__input").value === "" ||
+      document.querySelector(".repeatPasswordReg__input").value === ""
     ) {
-      peoples.push({
-        login: document.querySelector(".loginReg__input").value,
-        password: document.querySelector(".passwordReg__input").value,
-      });
-      regOperatingData = {
-        login: document.querySelector(".loginReg__input").value,
-        password: document.querySelector(".passwordReg__input").value,
-        balance: 0,
-        transactions: [],
-      };
-      localStorage.setItem(
-        document.querySelector(".loginReg__input").value,
-        JSON.stringify(regOperatingData)
-      );
-      document.querySelector(".loginLog__input").value = "";
-      document.querySelector(".passwordReg__input").value = "";
-      document.querySelector(".repeatPasswordReg__input").value = "";
-      document.querySelector(".passwordReg__input").style.border =
-        "1px solid black";
-      document.querySelector(".passwordReg__input").style.borderRadius = "2px";
-      document.querySelector(".repeatPasswordReg__input").style.border =
-        "1px solid black";
-      document.querySelector(".repeatPasswordReg__input").style.borderRadius =
-        "2px";
-      document.querySelector(".sighUp-block").classList.add("hidden");
-      document.querySelector(".signIn-block").classList.remove("hidden");
+      console.error("Error, please write!");
     } else {
-      document.querySelector(".passwordReg__input").style.border =
-        "1px solid red";
-      document.querySelector(".repeatPasswordReg__input").style.border =
-        "1px solid red";
+      if (
+        document.querySelector(".passwordReg__input").value ===
+        document.querySelector(".repeatPasswordReg__input").value
+      ) {
+        peoples.push({
+          login: document.querySelector(".loginReg__input").value,
+          password: document.querySelector(".passwordReg__input").value,
+        });
+        regOperatingData = {
+          login: document.querySelector(".loginReg__input").value,
+          password: document.querySelector(".passwordReg__input").value,
+          balance: 0,
+          transactions: [],
+        };
+        localStorage.setItem(
+          document.querySelector(".loginReg__input").value,
+          JSON.stringify(regOperatingData)
+        );
+        document.querySelector(".loginLog__input").value = "";
+        document.querySelector(".passwordReg__input").value = "";
+        document.querySelector(".repeatPasswordReg__input").value = "";
+        document.querySelector(".passwordReg__input").style.border =
+          "1px solid black";
+        document.querySelector(".passwordReg__input").style.borderRadius =
+          "2px";
+        document.querySelector(".repeatPasswordReg__input").style.border =
+          "1px solid black";
+        document.querySelector(".repeatPasswordReg__input").style.borderRadius =
+          "2px";
+        document.querySelector(".sighUp-block").classList.add("hidden");
+        document.querySelector(".signIn-block").classList.remove("hidden");
+      } else {
+        document.querySelector(".passwordReg__input").style.border =
+          "1px solid red";
+        document.querySelector(".repeatPasswordReg__input").style.border =
+          "1px solid red";
+      }
+      document.querySelector(".loginLog__input").value = "";
+      document.querySelector(".passwordLog__input").value = "";
     }
-    document.querySelector(".loginLog__input").value = "";
-    document.querySelector(".passwordLog__input").value = "";
   });
 
   btnLog.addEventListener("click", function () {
@@ -193,6 +202,8 @@ document.addEventListener("DOMContentLoaded", (e) => {
     btnLog.classList.remove("hidden");
     btnLogOut.classList.add("hidden");
     borrowBox.classList.add("hidden");
+    document.querySelector(".name-input").value = "";
+    document.querySelector(".send__money-input").value = "";
   });
 });
 
@@ -203,11 +214,47 @@ document
     let sendMoneyInputValue = Number(
       document.querySelector(".send__money-input").value
     );
-    let userObject = JSON.parse(localStorage.getItem(nameSendInput));
-    const dataBorrow = new Date();
-    let current_login = localStorage.getItem("current-login");
-    if (userObject !== null) {
-      let contentOfTransaction = `Time of transaction: ${
+    let check = localStorage.getItem("current-login");
+    let transactionsArr = JSON.parse(localStorage.getItem(check));
+    if (sendMoneyInputValue > transactionsArr.balance) {
+      document.querySelector(".send__money-input").style.borderColor = "red";
+    } else {
+      document.querySelector(".send__money-input").style.borderColor = "black";
+      let userObject = JSON.parse(localStorage.getItem(nameSendInput));
+      const dataBorrow = new Date();
+      let current_login = localStorage.getItem("current-login");
+      if (userObject !== null) {
+        // to
+        let contentOfTransaction = `Time of transaction: ${
+          dataBorrow.getHours() < 10
+            ? "0" + dataBorrow.getHours()
+            : dataBorrow.getHours()
+        }:${
+          dataBorrow.getMinutes() < 10
+            ? "0" + dataBorrow.getMinutes()
+            : dataBorrow.getMinutes()
+        }:${
+          dataBorrow.getSeconds() < 10
+            ? "0" + dataBorrow.getSeconds()
+            : dataBorrow.getSeconds()
+        }  ${
+          dataBorrow.getDate() < 10
+            ? "0" + dataBorrow.getDate()
+            : dataBorrow.getDate()
+        }.${
+          dataBorrow.getMonth() + 1 < 10
+            ? "0" + (dataBorrow.getMonth() + 1)
+            : dataBorrow.getMonth() + 1
+        }.${dataBorrow.getFullYear()} <br> Inbox amount: $${sendMoneyInputValue} from ${current_login}`;
+        userObject.balance += sendMoneyInputValue;
+        userObject.transactions.push(contentOfTransaction);
+        localStorage.setItem(nameSendInput, JSON.stringify(userObject));
+        document.querySelector(".name-input").value = "";
+        document.querySelector(".send__money-input").value = "";
+      }
+
+      //from
+      contentOfTransaction = `Time of transaction: ${
         dataBorrow.getHours() < 10
           ? "0" + dataBorrow.getHours()
           : dataBorrow.getHours()
@@ -227,49 +274,23 @@ document
         dataBorrow.getMonth() + 1 < 10
           ? "0" + (dataBorrow.getMonth() + 1)
           : dataBorrow.getMonth() + 1
-      }.${dataBorrow.getFullYear()} <br> Inbox amount: $${sendMoneyInputValue} from ${current_login}`;
-      userObject.balance += sendMoneyInputValue;
-      userObject.transactions.push(contentOfTransaction);
-      localStorage.setItem(nameSendInput, JSON.stringify(userObject));
-      document.querySelector(".name-input").textContent = "";
-      document.querySelector(".send__money-input").textContent = "";
+      }.${dataBorrow.getFullYear()} <br> Amount to somebody: -$${sendMoneyInputValue} to ${nameSendInput}`;
+      transactionsArr = JSON.parse(localStorage.getItem(current_login));
+      transactionsArr.balance -= sendMoneyInputValue;
+      localStorage.setItem(current_login, JSON.stringify(transactionsArr));
+      displayedBalance = localStorage.getItem(current_login);
+      displayedBalance = JSON.parse(displayedBalance);
+      document.querySelector(
+        ".current__balance"
+      ).innerHTML = `<h2>Current balance: $${displayedBalance.balance}</h2>`;
+      transactionsArr.transactions.push(contentOfTransaction);
+      localStorage.setItem(current_login, JSON.stringify(transactionsArr));
+      let transactionsData = JSON.parse(localStorage.getItem(current_login));
+      let newElementOfTransactions = "";
+      for (let i = 0; i < transactionsData.transactions.length; i++) {
+        newElementOfTransactions += `<div>${transactionsData.transactions[i]}</div>`;
+      }
+      document.querySelector(".transaction__box").innerHTML =
+        newElementOfTransactions;
     }
-    contentOfTransaction = `Time of transaction: ${
-      dataBorrow.getHours() < 10
-        ? "0" + dataBorrow.getHours()
-        : dataBorrow.getHours()
-    }:${
-      dataBorrow.getMinutes() < 10
-        ? "0" + dataBorrow.getMinutes()
-        : dataBorrow.getMinutes()
-    }:${
-      dataBorrow.getSeconds() < 10
-        ? "0" + dataBorrow.getSeconds()
-        : dataBorrow.getSeconds()
-    }  ${
-      dataBorrow.getDate() < 10
-        ? "0" + dataBorrow.getDate()
-        : dataBorrow.getDate()
-    }.${
-      dataBorrow.getMonth() + 1 < 10
-        ? "0" + (dataBorrow.getMonth() + 1)
-        : dataBorrow.getMonth() + 1
-    }.${dataBorrow.getFullYear()} <br> Amount to somebody: -$${sendMoneyInputValue} to ${nameSendInput}`;
-    transactionsArr = JSON.parse(localStorage.getItem(current_login));
-    transactionsArr.balance -= sendMoneyInputValue;
-    localStorage.setItem(current_login, JSON.stringify(transactionsArr));
-    displayedBalance = localStorage.getItem(current_login);
-    displayedBalance = JSON.parse(displayedBalance);
-    document.querySelector(
-      ".current__balance"
-    ).innerHTML = `<h2>Current balance: $${displayedBalance.balance}</h2>`;
-    transactionsArr.transactions.push(contentOfTransaction);
-    localStorage.setItem(current_login, JSON.stringify(transactionsArr));
-    let transactionsData = JSON.parse(localStorage.getItem(current_login));
-    let newElementOfTransactions = "";
-    for (let i = 0; i < transactionsData.transactions.length; i++) {
-      newElementOfTransactions += `<div>${transactionsData.transactions[i]}</div>`;
-    }
-    document.querySelector(".transaction__box").innerHTML =
-      newElementOfTransactions;
   });
